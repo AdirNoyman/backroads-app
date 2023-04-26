007e8ba581a95824c2976a11f722bf1a28519e65
Adir Noyman <ofahol2007@hotmail.com>
2023-04-26 19:25:46 +0300

    refactored the app to use more React components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../images/logo.svg';
-import { pageLinks } from '../data';
+import { pageLinks, socialLinks } from '../data';
 
 const NavBar = () => {
   return (
@@ -27,33 +27,21 @@ const NavBar = () => {
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  rel="noreferrer"
+                  target="_blank"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
